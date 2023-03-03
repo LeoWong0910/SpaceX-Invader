5a2beb9768f73473c275cd6ceaa21c26ea333f6a
h11mech017 <jackylv95237@gmail.com>
2023-03-03 16:21:38 +0800

    Just Testing

--- src/main/java/com/ood/spacexinvader/GameStart.java
+++ src/main/java/com/ood/spacexinvader/GameStart.java
@@ -4,19 +4,16 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.SceneFactory;
-import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.dsl.components.OffscreenCleanComponent;
 import com.almasb.fxgl.dsl.components.ProjectileComponent;
-import com.almasb.fxgl.dsl.handlers.CollectibleHandler;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.input.UserAction;
-import com.almasb.fxgl.physics.CollisionHandler;
 import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 import static com.ood.spacexinvader.GameConfig.*;
 
@@ -47,28 +44,12 @@ public class GameStart extends GameApplication {
     @Override
     protected void initGame() {
         shootTimer = FXGL.newLocalTimer();
-        //Spawn 30 Enemy
-        for (int i = 0; i < 30; i++) {
-            Enemy();
-        }
         player = FXGL.entityBuilder()
                 .type(EntityType.PLAYER)
                 .at((WIDTH - PLAYER_SIZE) / 2  , HEIGHT - PLAYER_SIZE )
                 .viewWithBBox(PLAYER_IMAGE)
                 .with(new CollidableComponent(true))
                 .buildAndAttach();
-    }
-
-    @Override
-    protected void initPhysics() {
-        FXGL.getPhysicsWorld().addCollisionHandler(
-                new CollisionHandler(EntityType.BULLET,EntityType.ENEMY) {
-                    @Override
-                    protected void onCollisionBegin(Entity bullet, Entity enemy) {
-                        bullet.removeFromWorld();
-                        enemy.removeFromWorld();
-                    }
-                });
     }
 
     @Override
@@ -110,9 +91,6 @@ public class GameStart extends GameApplication {
                         .at(player.getCenter().getX() - 4, player.getCenter().getY() - 50)
                         .viewWithBBox(new Rectangle(BULLET_SIZE,BULLET_SIZE, Color.BLACK))
                         .with(new ProjectileComponent(new Point2D(0, -2),BULLET_SPEED))
-                        .with(new OffscreenCleanComponent())
-                        .type(EntityType.BULLET)
-                        .collidable()
                         .buildAndAttach();
 
             }
@@ -122,16 +100,6 @@ public class GameStart extends GameApplication {
     @Override
     protected void onUpdate(double tpf) {
         isMoving = false;
-    }
-
-
-    private void Enemy(){
-        FXGL.entityBuilder()
-                .viewWithBBox(ALIEN_IMAGE)
-                .at(FXGLMath.random(-30,WIDTH-90), FXGLMath.random(0,ENEMY_SIZE))
-                .type(EntityType.ENEMY)
-                .collidable()
-                .buildAndAttach();
     }
 
     public static void main(String[] args) {
