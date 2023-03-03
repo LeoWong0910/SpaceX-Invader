ada677bf9dbe46df72eb6afc8ebfab3a0c8a573d
LeoWong <o0siu123210o@icloud.com>
2023-03-03 14:02:00 +0800

    optimised input

--- src/main/java/com/ood/spacexinvader/GameConfig.java
+++ src/main/java/com/ood/spacexinvader/GameConfig.java
@@ -6,6 +6,8 @@ public class GameConfig {
     public static final int WIDTH = 450;
     public static final double SPEED = 5f;
     public static final int PLAYER_SIZE = 100;
+
+    public static boolean isMoving;
     //follow by the FXGL structure.
     public static final String ICON = "icon/spacex-invader-favicon-black.png";
     public static final String PLAYER_IMAGE = "player/SpaceX.png";
