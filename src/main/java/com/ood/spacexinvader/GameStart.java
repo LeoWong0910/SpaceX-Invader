365fff74cbce0852b5d7bda069ac96ea249db557
h11mech017 <jackylv95237@gmail.com>
2023-03-03 11:37:35 +0800

    Added Main menu

--- src/main/java/com/ood/spacexinvader/GameStart.java
+++ src/main/java/com/ood/spacexinvader/GameStart.java
@@ -2,12 +2,8 @@ package com.ood.spacexinvader;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.entity.Entity;
-import com.almasb.fxgl.entity.components.CollidableComponent;
-import com.almasb.fxgl.input.UserAction;
-import javafx.scene.input.KeyCode;
-
+import com.almasb.fxgl.app.scene.FXGLMenu;
+import com.almasb.fxgl.app.scene.SceneFactory;
 
 public class GameStart extends GameApplication {
     @Override
@@ -15,40 +11,13 @@ public class GameStart extends GameApplication {
         settings.setTitle(GameConfig.Title);
         settings.setHeight(GameConfig.HEIGHT);
         settings.setWidth(GameConfig.WIDTH);
-
-        //assets/textures/spacex-invader-favicon-black.png
-        settings.setAppIcon(GameConfig.ICON);
-    }
-
-    //Override to Adding entity.
-    private Entity player;
-    @Override
-    protected void initGame() {
-        player = FXGL.entityBuilder()
-                .type(EntityType.PLAYER)
-                .at((GameConfig.WIDTH - GameConfig.PLAYER_SIZE) / 2  ,GameConfig.HEIGHT - GameConfig.PLAYER_SIZE )
-                .viewWithBBox(GameConfig.PLAYER_IMAGE)
-                .with(new CollidableComponent(true))
-                .buildAndAttach();
-
-
-    }
-
-    @Override
-    protected void initInput() {
-        FXGL.getInput().addAction(new UserAction("Move Left"){
+        settings.setMainMenuEnabled(true);
+        settings.setSceneFactory(new SceneFactory() {
             @Override
-            protected void onAction() {
-                player.translateX(-GameConfig.SPEED);
+            public FXGLMenu newMainMenu() {
+                return new MainMenu();
             }
-        } , KeyCode.LEFT);
-
-        FXGL.getInput().addAction(new UserAction("Move Right"){
-            @Override
-            protected void onAction() {
-                player.translateX(GameConfig.SPEED);
-            }
-        } , KeyCode.RIGHT);
+        });
     }
 
     public static void main(String[] args) {
