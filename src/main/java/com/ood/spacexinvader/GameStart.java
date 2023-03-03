93e5fe1353b7dc4789a56e85d3f7748e6d609753
h11mech017 <jackylv95237@gmail.com>
2023-03-03 11:37:35 +0800

    Added Main menu

--- src/main/java/com/ood/spacexinvader/GameStart.java
+++ src/main/java/com/ood/spacexinvader/GameStart.java
@@ -2,6 +2,8 @@ package com.ood.spacexinvader;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.scene.FXGLMenu;
+import com.almasb.fxgl.app.scene.SceneFactory;
 
 public class GameStart extends GameApplication {
     @Override
@@ -9,6 +11,13 @@ public class GameStart extends GameApplication {
         settings.setTitle(GameConfig.Title);
         settings.setHeight(GameConfig.HEIGHT);
         settings.setWidth(GameConfig.WIDTH);
+        settings.setMainMenuEnabled(true);
+        settings.setSceneFactory(new SceneFactory() {
+            @Override
+            public FXGLMenu newMainMenu() {
+                return new MainMenu();
+            }
+        });
     }
 
     public static void main(String[] args) {
