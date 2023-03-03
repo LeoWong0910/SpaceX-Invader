ad292e6c5b0e12473c414e5608abf43b3991d4e6
LeoWong <o0siu123210o@icloud.com>
2023-03-03 11:43:22 +0800

    change Entity

--- src/main/java/com/ood/spacexinvader/GameStart.java
+++ src/main/java/com/ood/spacexinvader/GameStart.java
@@ -4,6 +4,11 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.SceneFactory;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.input.UserAction;
+import javafx.scene.input.KeyCode;
 
 public class GameStart extends GameApplication {
     @Override
@@ -11,6 +16,8 @@ public class GameStart extends GameApplication {
         settings.setTitle(GameConfig.Title);
         settings.setHeight(GameConfig.HEIGHT);
         settings.setWidth(GameConfig.WIDTH);
+        settings.setAppIcon(GameConfig.ICON);
+
         settings.setMainMenuEnabled(true);
         settings.setSceneFactory(new SceneFactory() {
             @Override
@@ -19,6 +26,38 @@ public class GameStart extends GameApplication {
             }
         });
     }
+
+    //Override to Adding entity.
+    private Entity player;
+    @Override
+    protected void initGame() {
+        player = FXGL.entityBuilder()
+                .type(EntityType.PLAYER)
+                .at((GameConfig.WIDTH - GameConfig.PLAYER_SIZE) / 2  ,GameConfig.HEIGHT - GameConfig.PLAYER_SIZE )
+                .viewWithBBox(GameConfig.PLAYER_IMAGE)
+                .with(new CollidableComponent(true))
+                .buildAndAttach();
+
+
+    }
+
+    @Override
+    protected void initInput() {
+        FXGL.getInput().addAction(new UserAction("Move Left"){
+            @Override
+            protected void onAction() {
+                player.translateX(-GameConfig.SPEED);
+            }
+        } , KeyCode.LEFT);
+
+        FXGL.getInput().addAction(new UserAction("Move Right"){
+            @Override
+            protected void onAction() {
+                player.translateX(GameConfig.SPEED);
+            }
+        } , KeyCode.RIGHT);
+    }
+
 
     public static void main(String[] args) {
         launch(args);
