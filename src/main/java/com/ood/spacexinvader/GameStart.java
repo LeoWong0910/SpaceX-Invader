17c754509ded32567be04103abdaab633660e819
LeoWong <o0siu123210o@icloud.com>
2023-03-04 00:20:18 +0800

    Added Score and HP

--- src/main/java/com/ood/spacexinvader/GameStart.java
+++ src/main/java/com/ood/spacexinvader/GameStart.java
@@ -8,15 +8,18 @@ import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.dsl.components.OffscreenCleanComponent;
 import com.almasb.fxgl.dsl.components.ProjectileComponent;
-import com.almasb.fxgl.dsl.handlers.CollectibleHandler;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.physics.CollisionHandler;
+import com.almasb.fxgl.physics.HitBox;
 import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import java.util.Map;
 
 import static com.ood.spacexinvader.GameConfig.*;
 
@@ -67,6 +70,18 @@ public class GameStart extends GameApplication {
                     protected void onCollisionBegin(Entity bullet, Entity enemy) {
                         bullet.removeFromWorld();
                         enemy.removeFromWorld();
+                        FXGL.getWorldProperties().increment("score",+1);
+                    }
+                });
+        FXGL.getPhysicsWorld().addCollisionHandler(
+                new CollisionHandler(EntityType.ENEMY,EntityType.PLAYER) {
+                    @Override
+                    protected void onCollisionBegin(Entity enemy, Entity player) {
+                        enemy.removeFromWorld();
+                        if (FXGL.getWorldProperties().getInt("hp") <= 0){
+                            player.removeFromWorld();
+                        }
+                        FXGL.getWorldProperties().increment("hp",-1);;
                     }
                 });
     }
@@ -120,6 +135,38 @@ public class GameStart extends GameApplication {
     }
 
     @Override
+    protected void initUI() {
+        Text showScore = FXGL.getUIFactoryService().newText("Score: ",Color.BLACK, 22);
+        Text realScore = FXGL.getUIFactoryService().newText("",Color.BLACK, 22);
+
+        Text showHP = FXGL.getUIFactoryService().newText("HP: " ,Color.RED, 22);
+        Text realHP = FXGL.getUIFactoryService().newText("" ,Color.RED, 22);
+
+
+        showHP.setTranslateX(10);
+        showHP.setTranslateY(50);
+        realHP.setTranslateX(10 + 40);
+        realHP.setTranslateY(50);
+
+        showScore.setTranslateX(10);
+        showScore.setTranslateY(50 + 30);
+        realScore.setTranslateX(10 + 70);
+        realScore.setTranslateY(50 + 30);
+
+        realHP.textProperty().bind(FXGL.getWorldProperties().intProperty("hp").asString());
+        realScore.textProperty().bind(FXGL.getWorldProperties().intProperty("score").asString());
+
+        FXGL.getGameScene().addUINodes(showHP, showScore,realHP,realScore);
+
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("score",0);
+        vars.put("hp",20);
+    }
+
+    @Override
     protected void onUpdate(double tpf) {
         isMoving = false;
     }
@@ -130,6 +177,8 @@ public class GameStart extends GameApplication {
                 .viewWithBBox(ALIEN_IMAGE)
                 .at(FXGLMath.random(-30,WIDTH-90), FXGLMath.random(0,ENEMY_SIZE))
                 .type(EntityType.ENEMY)
+                .with(new ProjectileComponent(new Point2D(0, 1),ENEMY_SPEED))
+                .rotate(270)
                 .collidable()
                 .buildAndAttach();
     }
